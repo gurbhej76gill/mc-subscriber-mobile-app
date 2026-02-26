d1fb54c8b5d3be11ab4bba2193876423dd462770
gurbhej76gill <gurbhej76.gill@routerarchitects.com>
2026-02-26 15:26:34 +0530

    Resolved the review comment for mutation of model objects in list

--- lib/presentation/device_management_screen/models/mobile_device_info_model.dart
+++ lib/presentation/device_management_screen/models/mobile_device_info_model.dart
@@ -1,31 +1,45 @@
 /// This class is used in the [device_management_screen] screen.
-
-// ignore_for_file: must_be_immutable
 class MobileDeviceInfoModel {
   MobileDeviceInfoModel({
     required this.macAddress,
     this.deviceName = 'NA',
     this.uploadSpeed = 'NA',
     this.downloadSpeed = 'NA',
-    this.iconPath,
+    String? iconPath,
     this.isPaused = false,
     this.isPauseResumeInProgress = false,
     this.isHistoricalDevice = false,
-  }) {
-    macAddress = macAddress;
-    deviceName = deviceName;
-    uploadSpeed = uploadSpeed;
-    downloadSpeed = downloadSpeed;
-    iconPath = iconPath ?? 'assets/images/img_depth_4_frame_0_1.svg';
-    isPaused = isPaused;
-  }
+  }) : iconPath = iconPath ?? 'assets/images/img_depth_4_frame_0_1.svg';
 
-  String macAddress;
-  String deviceName;
-  String uploadSpeed;
-  String downloadSpeed;
-  String? iconPath;
-  bool isPaused;
-  bool isPauseResumeInProgress;
-  bool isHistoricalDevice;
+  final String macAddress;
+  final String deviceName;
+  final String uploadSpeed;
+  final String downloadSpeed;
+  final String? iconPath;
+  final bool isPaused;
+  final bool isPauseResumeInProgress;
+  final bool isHistoricalDevice;
+
+  MobileDeviceInfoModel copyWith({
+    String? macAddress,
+    String? deviceName,
+    String? uploadSpeed,
+    String? downloadSpeed,
+    String? iconPath,
+    bool? isPaused,
+    bool? isPauseResumeInProgress,
+    bool? isHistoricalDevice,
+  }) {
+    return MobileDeviceInfoModel(
+      macAddress: macAddress ?? this.macAddress,
+      deviceName: deviceName ?? this.deviceName,
+      uploadSpeed: uploadSpeed ?? this.uploadSpeed,
+      downloadSpeed: downloadSpeed ?? this.downloadSpeed,
+      iconPath: iconPath ?? this.iconPath,
+      isPaused: isPaused ?? this.isPaused,
+      isPauseResumeInProgress:
+          isPauseResumeInProgress ?? this.isPauseResumeInProgress,
+      isHistoricalDevice: isHistoricalDevice ?? this.isHistoricalDevice,
+    );
+  }
 }
